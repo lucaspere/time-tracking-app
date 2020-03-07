f56abc331e81d6fc60e387bce46cddef8dd2244b
lucaspere <lucasrupestre@hotmail.com>
2020-03-07 20:17:40 -0300

    definindo a funcionalidade de converter milisegundos em um formato HH:MM:SS

---
+++ utils/TimerUtils.js
@@ -0,0 +1,35 @@
+import uuidv4 from 'uuid/v4';
+
+ const millisecondsToHuman = ms => {
+   const seconds = Math.floor((ms / 1000) % 60);
+   const minutes = Math.floor((ms / 1000 / 60) % 60);
+   const hours = Math.floor((ms / 1000 / 60 / 60) % 60);
+
+   const humanized = [
+      pad(hours.toString(), 2),
+      pad(minutes.toString(), 2),
+      pad(seconds.toString(), 2),
+    ].join(':');
+
+   return humanized;
+};
+
+const pad = (numberString, size) => {
+   let padded = numberString;
+
+   while(padded.length < size) {
+      padded = `0${padded}`;
+   }
+
+   return padded;
+};
+
+export const newTimer = (attrs = {}) => {
+   const timer = {
+      title: attrs.title || 'Cronômetro',
+      project: attrs.project || 'Projeto',
+      id: uuidv4(),
+      elapsed: 0,
+      isRunning: false
+   };
+})
